bc5b31b6529e46007ce0c243249384b07ec70d6a
RenanSena2023 <renansena20152proton.me>
2023-06-25 02:44:27 -0300

    Classe Period :compara o tempo entre dois localDateTime/localDate e retorna o periodo decorrido. similar ao duration so que e usado para periodos de dias,meses e anos . duration usado para horas,minutos,segundos, milissegundos,nanosegundos

--- treinoAulaJava2/src/treinoAulaJava2/application/Main.java
+++ treinoAulaJava2/src/treinoAulaJava2/application/Main.java
@@ -3,25 +3,31 @@ package treinoAulaJava2.application;
 import java.text.ParseException;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
 	public static void main(String[] args) throws ParseException, InterruptedException {
 
-		Instant instInicial = Instant.now();
-
-		Thread.sleep(2000);/*pode ser usado para ver a duração de um processo qualquer que nao conhecemos*/
-
-		Instant instFinal = Instant.now();
-
+	LocalDate dataAntiga = LocalDate.of(2020,2,7);
 		
-		Duration dur = Duration.between(instInicial, instFinal);
-		
-		System.out.println("Duração entre nano segundos :"+dur.toNanos());
-		System.out.println("Duração em horas : " +dur.toHours());
-		System.out.println("Duração em mili segundos"+dur.toMillis());
-		System.out.println("Duração em minutos : "+dur.toMinutes());
-		
+	LocalDate dataNova = LocalDate.of(2023,5,17);
+	
+	 System.out.println("Data antiga é depois da data nova : "+ dataAntiga.isAfter(dataNova));
+	 System.out.println("Data antiga é antes da data nova : "+ dataAntiga.isBefore(dataNova));
+	 System.out.println("as Datas são iguais  : "+ dataAntiga.isEqual(dataNova));
+	 Period periodo = Period.between(dataAntiga, dataNova);
+	 
+	 System.out.println("Periodo entre as datas em meses : " + periodo.getMonths());
+	 System.out.println("Periodo entre as datas em dias : " + periodo.getDays());
+	 System.out.println("Periodo entre as datas em anos : " + periodo.getYears());
+	 
+	 System.out.println("periodo total : " + periodo.getYears()+" Anos "+
+						periodo.getMonths()+" Meses " + periodo.getDays()+" Dias");
+
+
 
 	}
 
