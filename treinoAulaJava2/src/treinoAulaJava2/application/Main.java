1d6ca8b3094382cfb33be8ae6a3352f1df9191ca
RenanSena2023 <renansena20152proton.me>
2023-06-25 01:26:56 -0300

    progrima para 12 parcelas

--- treinoAulaJava2/src/treinoAulaJava2/application/Main.java
+++ treinoAulaJava2/src/treinoAulaJava2/application/Main.java
@@ -11,12 +11,17 @@ public class Main {
 
 	public static void main(String[] args) throws ParseException {
 
-		Date dataPassada = new SimpleDateFormat("dd/MM/yyyy").parse("01/02/2021");
-		
+		Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse("10/04/2021");
 
-		long duração = ChronoUnit.DAYS.between( LocalDate.parse("2023-02-01"),LocalDate.now());
-		System.out.println("Passuí " + duração +" dias entre as duas datas ");
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dataInicial);
 		
+		for(int i = 1 ; i<= 12;i++) {
+			calendar.add(Calendar.MONTH, 1);
+			System.out.println("Parcela de número : " + i +" Vencimento é em : " + 
+			    new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+		}
+
 	}
 
 }
